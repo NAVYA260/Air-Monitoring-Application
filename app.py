d7596ed57c3a29473f2bf606cecf55f056820384
navyajangala123 <jangalanavyasri@gmail.com>
2025-01-01 16:33:13 +0530

    modified

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class User( db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(80), nullable=False)
     city = db.Column(db.String(120))
     is_admin = db.Column(db.Boolean, default=False) 
